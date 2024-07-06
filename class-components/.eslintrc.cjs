47421fa17857492060362817fb44c57514b302f7
Ivan-rom <rom_1van@icloud.com>
2024-07-06 15:04:15 +0600

    feat: implement development eviroment

--- class-components/.eslintrc.cjs
+++ class-components/.eslintrc.cjs
@@ -3,16 +3,35 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-compiler',
+    'react-hooks',
+    '@typescript-eslint',
+    'react-refresh',
+    'prettier',
+  ],
   rules: {
+    'react-compiler/react-compiler': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
